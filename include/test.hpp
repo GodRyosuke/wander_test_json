ab386a9563104fb42d9f76fede7a364ff45d5a74
GodRyosuke <ienvs0828@outlook.jp>
2021-12-30 10:27:15 +0900

    include error

--- include/test.hpp
+++ include/test.hpp
@@ -1,2 +1,14 @@
 #include "wander_lib/wander_lib.hpp"
 #include <json/json.h>
+
+class wander_test_class {
+public:
+	wander_test_class();
+
+	void hello();
+	void setdata(int data) { this->data = data; }
+	int get_data() { return data; }
+
+private:
+	int data;
+};
---
+++ src/main/test.cpp
@@ -0,0 +1,13 @@
+#include "test.hpp"
+#include <iostream>
+
+wander_test_class::wander_test_class()
+	:data(0)
+{
+
+}
+
+void wander_test_class::hello()
+{
+	std::cout << "hello" << std::endl;
+}
